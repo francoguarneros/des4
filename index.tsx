ae1b6d2b9961c666633d3f7037a40208c178be35
francoguarneros <francoguarmero@gmail.com>
2026-01-16 18:22:52 -0600

    Simplify root element rendering in index.tsx
    Refactor root element rendering logic for clarity.

--- index.tsx
+++ index.tsx
@@ -1,16 +1,12 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import App from './components/App'; // Esto está bien en tu imagen
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
