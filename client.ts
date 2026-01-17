0074105000267ef789dde74db51d1da47c8b9c5c
francoguarneros <francoguarmero@gmail.com>
2026-01-16 18:17:32 -0600

    Update client.ts

--- client.ts
+++ client.ts
@@ -3,5 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-// Esta es la variable que el resto del sistema busca
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
